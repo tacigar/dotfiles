a8f3e3b323f6e13b851032a8ab6182cc7b587ed8
tacigar <ig.lhound@gmail.com>
2018-04-22 11:07:48 +0900

    Use c++ google style

--- emacs/emacs.d.symlink/inits/21_init-cc.el
+++ emacs/emacs.d.symlink/inits/21_init-cc.el
@@ -1,8 +1,10 @@
 (add-hook 'c-mode-common-hook
 	  (lambda()
-	    (setq c-basic-offset 2
-		  tab-width 2
-		  c-default-style "linux"
+	    (setq c-basic-offset 4
+		  tab-width 4
 		  indent-tabs-mode nil)
 	    (c-set-offset 'innamespace 0)
 	    (c-set-offset 'case-label '+)))
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c++-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
