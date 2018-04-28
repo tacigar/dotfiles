49e8c665bf16dab460ec3513fdda39a794940d60
tacigar <ig.lhound@gmail.com>
2018-04-28 18:37:30 +0900

    Fix git-completion bugs

--- git/init.zsh
+++ git/init.zsh
@@ -3,8 +3,8 @@ function setup_completion() {
 
   mkdir -p $completion_dir
   
-  curl -o $completion_dir/git-completion.bash -O https://raw.github.com/git/git/master/contrib/completion/git-completion.bash 
-  curl -o $completion_dir/_git -O https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh 
+  wget -O $completion_dir/git-completion.bash https://raw.github.com/git/git/master/contrib/completion/git-completion.bash 
+  wget -O $completion_dir/_git https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh  
 
   rm -f $HOME/.zcompdump
 }
